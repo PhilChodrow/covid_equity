b2de86a03c4334a49e8e642b090a0e8695637bfc
Phil Chodrow <pchodrow@gmail.com>
2020-06-02 15:06:16 -0400

    add get_data function for retrieving scorecard data

---
+++ R/get_data.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+
+read_scorecard <- function(){
+	# test for presence of file in directory
+	data_dir <- "data/scorecard.csv"
+	if(file.exists(data_dir)){
+		print(paste("Reading scorecard data from", data_dir))
+		df <- read_csv(data_dir)
+		return(df)
+	}else{
+		if(!dir.exists('data')){
+			dir.create(file.path('data'), showWarnings = FALSE)
+		}
+		data_url <- "https://ed-public-download.app.cloud.gov/downloads/Most-Recent-Cohorts-All-Data-Elements.csv"
+		print(paste0("Downloading scorecard data from ", data_url, ". This might take a while."))
+		df <- read_csv(data_url)
+		df %>% 
+			write_csv('data/scorecard.csv')
+		return(df)
+	} 
+}
