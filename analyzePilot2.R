500f81de12216e877528f7148128887703e08d3c
Phil Chodrow <pchodrow@gmail.com>
2020-08-26 11:32:44 -0400

    tidy up analyzePilot2

--- analyzePilot2.R
+++ analyzePilot2.R
@@ -4,10 +4,14 @@
 library(tidyverse)
 library(fuzzyjoin)
 
+# Set path
+# path <- "~/Google Drive/Research/Projects/Current Research/PASS20/"
+
 # Load data
 
 googledata <- read_csv("data/pilotGoogleFormdata2.csv")
 mturkdata <- read_csv("data/pilotOutputData2.csv")
+
 
 # Clean data in google workerID
 names(googledata)[2] <- "WorkerId"
@@ -70,112 +74,134 @@ names(data)[27] <- "howappliesinstructorchoiceconfidence"
 # Replace any empty strings with NA
 data[data==""] <- NA
 
-# Recode prepandemic
-data$prepandemic <- as.factor(data$prepandemic)
-thelevels <- levels(data$prepandemic)
-data$prepandemic <- fct_recode(data$prepandemic, cannotdetermine = "Cannot determine")
-data$prepandemic <- fct_recode(data$prepandemic, letter = "Letter grades (typically, A/B/C/D/F, or a close variation thereof)")
-data$prepandemic <- fct_recode(data$prepandemic, pf = "Pass/Fail or High Pass/Pass/Fail or Satisfactory/Unsatisfactory (or a close variation thereof)")
-data$prepandemic <- fct_recode(data$prepandemic, pnc = "Pass/NoCredit or High Pass/Pass/No Credit (or a close variation thereof)")
+# Recode factors
 
-# Recode studentchoice
-data$studentchoice <- as.factor(data$studentchoice)
-thelevels <- levels(data$studentchoice)
-data$studentchoice <- fct_recode(data$studentchoice, nochoice = "No, despite the COVID pandemic, students were not given more choice than usual")
-data$studentchoice <- fct_recode(data$studentchoice, choice = "Yes, students were given more choice than usual due to the COVID pandemic")
-
-# Make studentchoiceconfidence
-data$studentchoiceconfidence <- rowSums(cbind(data$studentchoiceconfidence,data$studentnochoiceconfidence),na.rm=TRUE)
-drops <- c("studentnochoiceconfidence")
-data <- subset(data,select=setdiff(names(data),drops))
-
-# Recode prepandemicdefaultcompare
-data$prepandemicdefaultcompare <- as.factor(data$prepandemicdefaultcompare)
-thelevels <- levels(data$prepandemicdefaultcompare)
-data$prepandemicdefaultcompare <- fct_recode(data$prepandemicdefaultcompare, cannotdetermine = "Cannot determine")
-data$prepandemicdefaultcompare <- fct_recode(data$prepandemicdefaultcompare, same = "The pandemic default method was the same as the usual pre-pandemic method")
-data$prepandemicdefaultcompare <- fct_recode(data$prepandemicdefaultcompare, different = "The pandemic default method was different from the usual pre-pandemic method")
-
-# Make prepandemicdefaultcompareconfidence
-data$prepandemicdefaultcompareconfidence <- rowSums(cbind(data$prepandemicdifferentconfidence,data$prepandemicsameconfidence),na.rm=TRUE)
-drops <- c("prepandemicdifferentconfidence","prepandemicsameconfidence")
-data <- subset(data,select=setdiff(names(data),drops))
-
-# Recode pandemicdefault
-data$pandemicdefault <- as.factor(data$pandemicdefault)
-thelevels <- levels(data$pandemicdefault)
-data$pandemicdefault <- fct_recode(data$pandemicdefault, letter = "Averages are computed in grade points. Each graded semester hour of academic credit carries a corresponding number of grade points.")
-data$pandemicdefault <- fct_recode(data$pandemicdefault, cannotdetermine = "Cannot determine")
-data$pandemicdefault <- fct_recode(data$pandemicdefault, letter = "Letter grades (typically, A/B/C/D/F, or a close variation thereof)")
-data$pandemicdefault <- fct_recode(data$pandemicdefault, letter = "Letter grades with the inclusion of a Late Withdrawal (W) and No Credit (NC)")
-data$pandemicdefault <- fct_recode(data$pandemicdefault, pf = "Pass/Fail or High Pass/Pass/Fail or Satisfactory/Unsatisfactory (or a close variation thereof)")
-data$pandemicdefault <- fct_recode(data$pandemicdefault, pnc = "Pass/NoCredit or High Pass/Pass/No Credit (or a close variation thereof)")
-
-# Recode pandemicoptin
-data$pandemicoptin <- as.factor(data$pandemicoptin)
-thelevels <- levels(data$pandemicoptin)
-data$pandemicoptin <- fct_recode(data$pandemicoptin, cannotdetermine = "Cannot determine")
-data$pandemicoptin <- fct_recode(data$pandemicoptin, cw = "Covid Withdrawal")
-data$pandemicoptin <- fct_recode(data$pandemicoptin, pf = "For Spring 2020, students will have two weeks after grades are published to submit a petition to change a class from standard letter grade (A,B,C,D,F) to Pass/D/Fail.")
-data$pandemicoptin <- fct_recode(data$pandemicoptin, cwnc = "Late Withdrawal (W) and No Credit (NC)")
-data$pandemicoptin <- fct_recode(data$pandemicoptin, letter = "Letter grades (typically, A/B/C/D/F, or a close variation thereof)")
-data$pandemicoptin <- fct_recode(data$pandemicoptin, pf = "Pass/Fail")
-data$pandemicoptin <- fct_recode(data$pandemicoptin, pf = "Pass/Fail or High Pass/Pass/Fail or Satisfactory/Unsatisfactory (or a close variation thereof)")
-data$pandemicoptin <- fct_recode(data$pandemicoptin, pnc = "Pass/NoCredit or High Pass/Pass/No Credit (or a close variation thereof)")
-data$pandemicoptin <- fct_recode(data$pandemicoptin, pf = "Satisfactory/Satisfactory D/Fail")
-
-# Recode howappliesstudentchoice
-data$howappliesstudentchoice <- as.factor(data$howappliesstudentchoice)
-thelevels <- levels(data$howappliesstudentchoice)
-data$howappliesstudentchoice <- fct_recode(data$howappliesstudentchoice, cannotdetermine = "Cannot determine")
-data$howappliesstudentchoice <- fct_recode(data$howappliesstudentchoice, singlechoice = "No: the student was required to make a single choice (pandemic default or pandemic opt-in) which was applied to all courses")
-data$howappliesstudentchoice <- fct_recode(data$howappliesstudentchoice, partialpercourse = "Partially: Some courses allowed students to make a choice between the pandemic default method and the pandemic opt-in method, while for other courses, the instructor or institution chose the grading method")
-data$howappliesstudentchoice <- fct_recode(data$howappliesstudentchoice, other = "Some classes moved to a pass / fail grading system, but students have to log in and see if their particular class is affected. In order to access this information a person needs a log in and password." )
-data$howappliesstudentchoice <- fct_recode(data$howappliesstudentchoice, partialpercourse = "Students could only select up to two courses to change to pass/fail")
-data$howappliesstudentchoice <- fct_recode(data$howappliesstudentchoice, partialpercourse = "The students could only change up to two of their Spring Semester 2020 courses to Pass/Fail within a given time period.")
-data$howappliesstudentchoice <- fct_recode(data$howappliesstudentchoice, partialpercourse = "They could make up to two courses Pass/Fail")
-data$howappliesstudentchoice <- fct_recode(data$howappliesstudentchoice, fullchoice = "Yes: the student could choose between the pandemic default method and pandemic opt-in method for each of their courses, independently")
-data$howappliesstudentchoice[data$howappliesstudentchoice=="The student doesn't have a choice"] <- NA
-
-# Recode deadline
-data$deadline <- as.factor(data$deadline)
-thelevels <- levels(data$deadline)
-data$deadline <- fct_recode(data$deadline, after = "After they saw what their grade would be using the pandemic default method")
-data$deadline <- fct_recode(data$deadline, before = "Before they saw what their grade would be under the pandemic default method")
-data$deadline <- fct_recode(data$deadline, cannotdetermine = "Cannot determine")
-goodvals <- c("after","before","cannotdetermine")
-data$deadline[!data$deadline %in% goodvals] <- NA
-data$deadline <- factor(data$deadline)
-
-# Recode prepandemicpandemiccompare
-data$prepandemicpandemiccompare <- as.factor(data$prepandemicpandemiccompare)
-thelevels <- levels(data$prepandemicpandemiccompare)
-data$prepandemicpandemiccompare <- fct_recode(data$prepandemicpandemiccompare, cannotdetermine = "Cannot determine")
-data$prepandemicpandemiccompare <- fct_recode(data$prepandemicpandemiccompare, same = "The pandemic grading method was the same as the usual pre-pandemic grading method" )
-data$prepandemicpandemiccompare <- fct_recode(data$prepandemicpandemiccompare, different = "The pandemic grading method was different from the usual pre-pandemic grading method")
-
-# Make prepandemicpandemiccompareconfidence
-data$prepandemicpandemiccompareconfidence <- rowSums(cbind(data$pandemicdifferentconfidence,data$pandemicsameconfidence),na.rm=TRUE)
-drops <- c("pandemicdifferentconfidence","pandemicsameconfidence")
-data <- subset(data,select=setdiff(names(data),drops))
-
+data <- data %>% 
+	tibble() %>% 
+	# recode prepandemic
+	mutate(prepandemic = factor(prepandemic),
+				 prepandemic = fct_recode(prepandemic,
+					 	cannotdetermine = "Cannot determine",
+					 	letter          = "Letter grades (typically, A/B/C/D/F, or a close variation thereof)",
+					 	pf              = "Pass/Fail or High Pass/Pass/Fail or Satisfactory/Unsatisfactory (or a close variation thereof)",
+					 	pnc             = "Pass/NoCredit or High Pass/Pass/No Credit (or a close variation thereof)"
+				 													)
+	) %>% 
+	# recode studentchoice
+	mutate(studentchoice = factor(studentchoice),
+				 studentchoice = fct_recode(studentchoice,
+				 		nochoice = "No, despite the COVID pandemic, students were not given more choice than usual",
+				 		choice   = "Yes, students were given more choice than usual due to the COVID pandemic"
+				 														)
+	) %>% 
+ 	# make studentchoiceconfidence
+	rowwise() %>% 
+ 	mutate(studentchoiceconfidence = sum(studentchoiceconfidence, studentnochoiceconfidence, na.rm = T)) %>% 
+	ungroup() %>% 
+	select(-studentnochoiceconfidence) %>% 
+	# recode prepandemicdefaultcompare
+	mutate(
+		prepandemicdefaultcompare = factor(prepandemicdefaultcompare),
+		prepandemicdefaultcompare = fct_recode(prepandemicdefaultcompare,
+				 cannotdetermine = "Cannot determine",
+				 same            = "The pandemic default method was the same as the usual pre-pandemic method",
+				 different       = "The pandemic default method was different from the usual pre-pandemic method"
+																					 )
+	) %>% 
+	# Make prepandemicdefaultcompareconfidence
+	rowwise() %>% 
+	mutate(prepandemicdefaultcompareconfidence = sum(prepandemicdifferentconfidence, prepandemicsameconfidence, na.rm = T)) %>% 
+	ungroup() %>% 
+	select(-prepandemicdifferentconfidence, -prepandemicsameconfidence) %>% 
+	# Recode pandemicdefault
+	mutate(pandemicdefault = factor(pandemicdefault),
+				 pandemicdefault = fct_recode(pandemicdefault,
+				 														 letter = "Averages are computed in grade points. Each graded semester hour of academic credit carries a corresponding number of grade points.",
+				 														 cannotdetermine = "Cannot determine",
+				 														 letter = "Letter grades (typically, A/B/C/D/F, or a close variation thereof)",
+				 														 letter = "Letter grades with the inclusion of a Late Withdrawal (W) and No Credit (NC)",
+				 														 pf = "Pass/Fail or High Pass/Pass/Fail or Satisfactory/Unsatisfactory (or a close variation thereof)",
+				 														 pnc = "Pass/NoCredit or High Pass/Pass/No Credit (or a close variation thereof)"
+				 														 )
+				 ) %>% 
+# Recode pandemicoptin 
+	mutate(pandemicoptin = factor(pandemicoptin),
+				 pandemicoptin = fct_recode(pandemicoptin, 
+				 													 cannotdetermine = "Cannot determine",
+				 													 cw = "Covid Withdrawal",
+				 													 pf = "For Spring 2020, students will have two weeks after grades are published to submit a petition to change a class from standard letter grade (A,B,C,D,F) to Pass/D/Fail.",
+				 													 cwnc = "Late Withdrawal (W) and No Credit (NC)",
+				 													 letter = "Letter grades (typically, A/B/C/D/F, or a close variation thereof)",
+				 													 pf = "Pass/Fail",
+				 													 pf = "Pass/Fail or High Pass/Pass/Fail or Satisfactory/Unsatisfactory (or a close variation thereof)",
+				 													 pnc = "Pass/NoCredit or High Pass/Pass/No Credit (or a close variation thereof)",
+				 													 pf = "Satisfactory/Satisfactory D/Fail"
+				 													 )
+	) %>% 
+	# Recode howappliesstudentchoice
+	mutate(howappliesstudentchoice = ifelse(howappliesstudentchoice == "The student doesn't have a choice", NA, howappliesstudentchoice),
+		howappliesstudentchoice = factor(howappliesstudentchoice),
+				 howappliesstudentchoice = fct_recode(howappliesstudentchoice,
+				 																		 cannotdetermine = "Cannot determine",
+				 																		 singlechoice = "No: the student was required to make a single choice (pandemic default or pandemic opt-in) which was applied to all courses",
+				 																		 partialpercourse = "Partially: Some courses allowed students to make a choice between the pandemic default method and the pandemic opt-in method, while for other courses, the instructor or institution chose the grading method",
+				 																		 other = "Some classes moved to a pass / fail grading system, but students have to log in and see if their particular class is affected. In order to access this information a person needs a log in and password.",
+				 																		 partialpercourse = "Students could only select up to two courses to change to pass/fail",
+				 																		 partialpercourse = "The students could only change up to two of their Spring Semester 2020 courses to Pass/Fail within a given time period.",
+				 																		 partialpercourse = "They could make up to two courses Pass/Fail",
+				 																		 fullchoice = "Yes: the student could choose between the pandemic default method and pandemic opt-in method for each of their courses, independently"
+				 																		 )) %>% 
+# Recode deadline (NEED TO FIX THIS)
+	mutate(deadline = ifelse(deadline %in% c(
+		"After they saw what their grade would be using the pandemic default method",
+		"Before they saw what their grade would be under the pandemic default method",
+		"Cannot determine"), deadline, NA),
+		deadline = factor(deadline),
+				 deadline = fct_recode(deadline,
+				 		after = "After they saw what their grade would be using the pandemic default method",
+				 		before = "Before they saw what their grade would be under the pandemic default method",
+				 		cannotdetermine = "Cannot determine",
+				 											)
+				) %>% 
+	# Recode prepandemicpandemiccompare
+	mutate(prepandemicpandemiccompare = factor(prepandemicpandemiccompare),
+				 prepandemicpandemiccompare = fct_recode(prepandemicpandemiccompare,
+				 																				cannotdetermine = "Cannot determine",
+				 																				same = "The pandemic grading method was the same as the usual pre-pandemic grading method",
+				 																				different = "The pandemic grading method was different from the usual pre-pandemic grading method"
+				 																				)
+	) %>% 
+	rowwise() %>% 
+	mutate(prepandemicpandemiccompareconfidence = sum(pandemicdifferentconfidence, pandemicsameconfidence, na.rm = T)) %>% 
+	ungroup() %>% 
+	select(-pandemicdifferentconfidence, -pandemicsameconfidence) %>% 
 # Recode pandemic
-data$pandemic <- as.factor(data$pandemic)
-thelevels <- levels(data$pandemic)
-data$pandemic <- fct_recode(data$pandemic, cannotdetermine = "Cannot determine")
-data$pandemic <- fct_recode(data$pandemic, letter = "Letter grades (typically, A/B/C/D/F, or a close variation thereof)")
-data$pandemic <- fct_recode(data$pandemic, pf = "Pass/Fail or High Pass/Pass/Fail or Satisfactory/Unsatisfactory (or a close variation thereof)")
-data$pandemic <- fct_recode(data$pandemic, pnc = "Pass/NoCredit or High Pass/Pass/No Credit (or a close variation thereof)")
-goodvals <- c("cannotdetermine","letter","pf","pnc")
-data$pandemic[!data$pandemic %in% goodvals] <- NA
-data$pandemic <- factor(data$pandemic)
-
+	mutate(
+		pandemic = ifelse(pandemic %in% c(
+			"Cannot determine",
+			"Letter grades (typically, A/B/C/D/F, or a close variation thereof)",
+			"Pass/Fail or High Pass/Pass/Fail or Satisfactory/Unsatisfactory (or a close variation thereof)",
+			"Pass/NoCredit or High Pass/Pass/No Credit (or a close variation thereof)"), 
+			pandemic, NA),
+		pandemic = factor(pandemic),
+		pandemic = fct_recode(pandemic,
+													cannotdetermine = "Cannot determine",
+													letter = "Letter grades (typically, A/B/C/D/F, or a close variation thereof)",
+													pf = "Pass/Fail or High Pass/Pass/Fail or Satisfactory/Unsatisfactory (or a close variation thereof)",
+													pnc = "Pass/NoCredit or High Pass/Pass/No Credit (or a close variation thereof)",
+		)
+	) %>% 
 # Recode howappliesinstructorchoice
-data$howappliesinstructorchoice <- as.factor(data$howappliesinstructorchoice)
-thelevels <- levels(data$howappliesinstructorchoice)
-data$howappliesinstructorchoice <- fct_recode(data$howappliesinstructorchoice, cannotdetermine = "Cannot determine")
-data$howappliesinstructorchoice <- fct_recode(data$howappliesinstructorchoice, instructorchoice = "Each course instructor could choose if the pandemic grading method would apply to their course")
-data$howappliesinstructorchoice <- fct_recode(data$howappliesinstructorchoice, institutionchoice = "Instructors had no choice, meaning that one single pandemic grading method chosen by the college/university automatically applied to all courses")
+	mutate(howappliesinstructorchoice = factor(howappliesinstructorchoice),
+				 howappliesinstructorchoice = fct_recode(howappliesinstructorchoice,
+				 																				cannotdetermine = "Cannot determine",
+				 																				instructorchoice = "Each course instructor could choose if the pandemic grading method would apply to their course",
+				 																				institutionchoice = "Instructors had no choice, meaning that one single pandemic grading method chosen by the college/university automatically applied to all courses"
+				 																				)
+				 )
+
+data %>% write_csv("throughput/pilot3.csv")
+
 
 
 # Put columns in order
